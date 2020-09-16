d2af531784dc61e72d212d8649ae31527db0bcae
Bilal Doğan <bilal.dogan@loodos.com>
2020-09-16 16:10:53 +0300

    Add compiled framework

---
+++ Crowbar.framework/Headers/Crowbar.h
@@ -0,0 +1,19 @@
+//
+//  Crowbar.h
+//  Crowbar
+//
+//  Created by Bilal Doğan on 7.05.2020.
+//  Copyright © 2020 Bilal Doğan. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Crowbar.
+FOUNDATION_EXPORT double CrowbarVersionNumber;
+
+//! Project version string for Crowbar.
+FOUNDATION_EXPORT const unsigned char CrowbarVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Crowbar/PublicHeader.h>
+
+
